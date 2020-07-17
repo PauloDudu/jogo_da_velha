f8422fe53a0219a79ac052d5fb2b41833d0cd511
PedroAugustto <augusttochaves@hotmail.com>
2020-07-17 19:57:55 -0300

    .

---
+++ jogo-da-velha.js
@@ -0,0 +1,16 @@
+
+        var linha = []
+        var coluna = []
+        
+        for(i = 0; i <= 3; i++ ){     
+            
+            for(c = 0; c <= 3; c++){
+
+                document.write(i,c)
+
+            };
+        
+
+        }; 
+
+       
